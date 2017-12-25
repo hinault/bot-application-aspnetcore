29330c2cd7f0fa6ed07d7dd9402434a903215d62
Hinault <ngdonfack@gmail.com>
2017-12-25 12:11:49 -0500

    Update MessagesController

--- BotApplication/Controllers/MessagesController.cs
+++ BotApplication/Controllers/MessagesController.cs
@@ -28,6 +28,27 @@ namespace BotApplication.Controllers
             return response;
         }
 
+        [Authorize(Roles = "Bot")]
+        [HttpPost]
+        public async Task<OkResult> Post([FromBody] Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                //MicrosoftAppCredentials.TrustServiceUrl(activity.ServiceUrl);
+                var appCredentials = new MicrosoftAppCredentials(configuration);
+                var connector = new ConnectorClient(new Uri(activity.ServiceUrl), appCredentials);
+
+                // return our reply to the user
+                var reply = activity.CreateReply("HelloWorld");
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else
+            {
+                //HandleSystemMessage(activity);
+            }
+            return Ok();
+        }
+
         private Activity HandleSystemMessage(Activity message)
         {
             if (message.Type == ActivityTypes.DeleteUserData)
