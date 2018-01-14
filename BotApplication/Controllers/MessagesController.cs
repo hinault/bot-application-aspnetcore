ab6bb6113b641953f82d9172df9ca53459df1296
Hinault <ngdonfack@gmail.com>
2018-01-14 08:49:43 -0500

    Remove Dialogs

--- BotApplication/Controllers/MessagesController.cs
+++ BotApplication/Controllers/MessagesController.cs
@@ -1,81 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Net.Http;
+using Microsoft.Bot.Connector;
+using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+//using System.Web.Http;
 
 namespace BotApplication.Controllers
 {
-    [BotAuthentication]
-    public class MessagesController : ApiController
+    //[BotAuthentication]
+    [Route("api/messages")]
+    public class MessagesController : Controller
     {
-        /// <summary>
-        /// POST: api/Messages
-        /// Receive a message from a user and reply to it
-        /// </summary>
-        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
-        {
-            if (activity.Type == ActivityTypes.Message)
-            {
-                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
-            }
-            else
-            {
-                HandleSystemMessage(activity);
-            }
-            var response = Request.CreateResponse(HttpStatusCode.OK);
-            return response;
-        }
 
-        [Authorize(Roles = "Bot")]
-        [HttpPost]
-        public async Task<OkResult> Post([FromBody] Activity activity)
-        {
-            if (activity.Type == ActivityTypes.Message)
-            {
-                //MicrosoftAppCredentials.TrustServiceUrl(activity.ServiceUrl);
-                var appCredentials = new MicrosoftAppCredentials(configuration);
-                var connector = new ConnectorClient(new Uri(activity.ServiceUrl), appCredentials);
+     private IConfiguration configuration;
 
-                // return our reply to the user
-                var reply = activity.CreateReply("HelloWorld");
-                await connector.Conversations.ReplyToActivityAsync(reply);
-            }
-            else
-            {
-                //HandleSystemMessage(activity);
-            }
-            return Ok();
-        }
+public MessagesController(IConfiguration configuration)
+{
+    this.configuration = configuration;
+}
 
-        private Activity HandleSystemMessage(Activity message)
-        {
-            if (message.Type == ActivityTypes.DeleteUserData)
-            {
-                // Implement user deletion here
-                // If we handle user deletion, return a real message
-            }
-            else if (message.Type == ActivityTypes.ConversationUpdate)
-            {
-                // Handle conversation state changes, like members being added and removed
-                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
-            }
-            else if (message.Type == ActivityTypes.ContactRelationUpdate)
-            {
-                // Handle add/remove from contact lists
-                // Activity.From + Activity.Action represent what happened
-            }
-            else if (message.Type == ActivityTypes.Typing)
-            {
-                // Handle knowing tha the user is typing
-            }
-            else if (message.Type == ActivityTypes.Ping)
-            {
-            }
 
-            return null;
-        }
+[Authorize(Roles = "Bot")]
+[HttpPost]
+public async Task<OkResult> Post([FromBody] Activity activity)
+{
+    if (activity.Type == ActivityTypes.Message)
+    {
+        //MicrosoftAppCredentials.TrustServiceUrl(activity.ServiceUrl);
+        var appCredentials = new MicrosoftAppCredentials(configuration);
+        var connector = new ConnectorClient(new Uri(activity.ServiceUrl), appCredentials);
+
+        // return our reply to the user
+        var reply = activity.CreateReply("HelloWorld");
+        await connector.Conversations.ReplyToActivityAsync(reply);
+    }
+    else
+    {
+        //HandleSystemMessage(activity);
+    }
+    return Ok();
+}
+
+      
     }
 }
